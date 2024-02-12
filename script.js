c31e43b758fa17163136c45f56ad873103430349
Duda Nascimento <mariaeduarda6n@gmail.com>
2024-02-12 11:59:19 -0300

    enviando arquivos

---
+++ script.js
@@ -0,0 +1,28 @@
+$('.galpao01').click(function(){
+    $('.wrapper .menu .itensGalpao01').toggleClass('mostra');
+    $('.wrapper .menu ol li .expand-more').toggleClass('mudar');
+});
+
+$('galpao01').click(function(){
+    $('.wrapper .menu ol li .expand-lass').toggleClass()
+});
+
+$('.galpao02').click(function(){
+    $('.wrapper .menu .itensGalpao02').toggleClass('mostra');
+
+});
+
+$('.graficos').click(function(){
+    $('.wrapper .menu .itensGraficos').toggleClass('mostra');
+
+});
+
+$('.temperaturas').click(function(){
+    $('.wrapper .menu .itensTemperaturas').toggleClass('mostra');
+
+});
+
+$('.vacinas').click(function(){
+    $('.wrapper .menu .itensVacinas').toggleClass('mostra');
+
+});
